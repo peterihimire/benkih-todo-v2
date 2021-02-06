268ff01716d1d9b6b8fc990ad1d51c5a1d37fb01
Peter Ihimire <peterihimire@gmail.com>
2021-02-06 01:03:13 +0100

    Working on my today date display

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,11 @@
-console.log("I want to start the javascript");
+const todoDate = document.querySelector(".todo-date");
+console.log(todoDate);
+
+const dateTimeFormat = (date) => {
+  // const date = new Date();
+
+  let minutes = date.getMinutes().toString().length == 1
+  console.log(minutes);
+  todoDate.textContent = date;
+};
+dateTimeFormat(new Date());
