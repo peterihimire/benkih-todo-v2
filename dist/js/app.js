484d26113dfb7f142d8b296d11b400280b8d2d04
Peter Ihimire <peterihimire@gmail.com>
2021-02-06 01:54:40 +0100

    Still working on the date

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,8 +4,14 @@ console.log(todoDate);
 const dateTimeFormat = (date) => {
   // const date = new Date();
 
-  let minutes = date.getMinutes().toString().length == 1
-  console.log(minutes);
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = date.getDay();
+  let month = date.getMonth()
+  let year = date.getYear()
+
+
+  console.log(minutes, hours, days,month, year);
   todoDate.textContent = date;
 };
 dateTimeFormat(new Date());
