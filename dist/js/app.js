d4aec5135eda2b7759f1b13c48b5ef9e55a96fd9
Peter Ihimire <peterihimire@gmail.com>
2021-02-06 13:12:45 +0100

    Modified the look a little, added date to the today heading and other ui minor changes

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,15 +3,38 @@ console.log(todoDate);
 
 const dateTimeFormat = (date) => {
   // const date = new Date();
+  let minutes = date.getMinutes().toString();
+  minutes = minutes < 10 ? "0" + minutes : minutes;
 
-  let minutes = date.getMinutes();
   let hours = date.getHours();
-  let days = date.getDay();
-  let month = date.getMonth()
-  let year = date.getYear()
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour 0 is converted to 12
+  let ampm = hours >= 12 ? "pm" : "am";
 
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  console.log(minutes, hours, days,month, year);
-  todoDate.textContent = date;
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let year = date.getFullYear().toString();
+
+  let time = `${hours} : ${minutes} ${ampm}`;
+  let today = `${days[date.getDay()]}. ${
+    months[date.getMonth()]
+  }. ${date.getDay().toString()}, ${year} `;
+
+  todoDate.firstElementChild.textContent = today;
 };
 dateTimeFormat(new Date());
