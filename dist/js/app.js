dfb090e7b28134ad97910c9d1b9e73ca7ca1c23f
Peter Ihimire <peterihimire@gmail.com>
2021-02-09 08:05:48 +0100

    Refactored the code , still working on the drag and drop

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,24 +5,83 @@ const todoSubmit = document.querySelector("#todo-submit");
 const todoDisplay = document.querySelector("#todo-display");
 const todoBox = document.querySelector("#todo-box");
 const formWrapper = document.querySelector("#form-wrapper");
-// var btn = document.querySelector(".add");
 const remove = document.querySelector(".draggable");
 const todoItem = document.querySelector(".item");
 console.log(todoItem);
 let todoItemList = [];
 let todoID = 0;
 
-// console.log(
-//   todoDate,
-//   todoInput,
-//   todoSubmit,
-//   todoDisplay,
-//   todoBox,
-//   formWrapper,
-//   todoFeedback
-// );
-
 // For Drag and Drop
+function dragStart(e) {
+  this.style.opacity = "0.4";
+  dragSrcEl = this;
+  console.log(this);
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.setData("text/html", this.innerHTML);
+}
+
+function dragEnter(e) {
+  this.classList.add("over");
+}
+
+function dragLeave(e) {
+  e.stopPropagation();
+  this.classList.remove("over");
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  e.dataTransfer.dropEffect = "move";
+  return false;
+}
+
+function dragDrop(e) {
+  if (dragSrcEl != this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData("text/html");
+  }
+  return false;
+}
+
+function dragEnd(e) {
+  var listItens = document.querySelectorAll(".draggable");
+  [].forEach.call(listItens, function (item) {
+    item.classList.remove("over");
+  });
+  this.style.opacity = "1";
+}
+
+function addEventsDragAndDrop(el) {
+  el.addEventListener("dragstart", dragStart, false);
+  el.addEventListener("dragenter", dragEnter, false);
+  el.addEventListener("dragover", dragOver, false);
+  el.addEventListener("dragleave", dragLeave, false);
+  el.addEventListener("drop", dragDrop, false);
+  el.addEventListener("dragend", dragEnd, false);
+}
+
+var listItens = document.querySelectorAll(".draggable");
+[].forEach.call(listItens, function (item) {
+  addEventsDragAndDrop(item);
+});
+
+// function addNewItem() {
+//   var newItem = document.querySelector(".input").value;
+//   if (newItem != "") {
+//     document.querySelector(".input").value = "";
+//     var li = document.createElement("li");
+//     var attr = document.createAttribute("draggable");
+//     var ul = document.querySelector("ul");
+//     li.className = "draggable";
+//     attr.value = "true";
+//     li.setAttributeNode(attr);
+//     li.appendChild(document.createTextNode(newItem));
+//     ul.appendChild(li);
+//     addEventsDragAndDrop(li);
+//   }
+// }
+
+// btn.addEventListener("click", addNewItem);
 
 // console.log(remove);
 
@@ -209,59 +268,5 @@ document.addEventListener("DOMContentLoaded", () => {
     if (e.target.classList.contains("todo-check")) {
       checkTodo(e.target);
     }
-  });
-
-  // Drag and Drop function
-  function dragStart(e) {
-    todoItem.style.opacity = "0.4";
-    dragSrcEl = todoItem;
-    console.log(dragSrcEl);
-    e.dataTransfer.effectAllowed = "move";
-    e.dataTransfer.setData("text/html", todoItem.innerHTML);
-  }
-  // dragStart();
-  function dragEnter(e) {
-    todoItem.classList.add("over");
-  }
-
-  function dragLeave(e) {
-    e.stopPropagation();
-    todoItem.classList.remove("over");
-  }
-
-  function dragOver(e) {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = "move";
-    return false;
-  }
-
-  function dragDrop(e) {
-    if (dragSrcEl != todoItem) {
-      dragSrcEl.innerHTML = todoItem.innerHTML;
-      todoItem.innerHTML = e.dataTransfer.getData("text/html");
-    }
-    return false;
-  }
-
-  function dragEnd(e) {
-    var listItens = document.querySelectorAll(".draggable");
-    [].forEach.call(listItens, function (item) {
-      item.classList.remove("over");
-    });
-    todoItem.style.opacity = "1";
-  }
-
-  function addEventsDragAndDrop(el) {
-    el.addEventListener("dragstart", dragStart, false);
-    el.addEventListener("dragenter", dragEnter, false);
-    el.addEventListener("dragover", dragOver, false);
-    el.addEventListener("dragleave", dragLeave, false);
-    el.addEventListener("drop", dragDrop, false);
-    el.addEventListener("dragend", dragEnd, false);
-  }
-
-  var listItens = document.querySelectorAll(".draggable");
-  [].forEach.call(listItens, function (item) {
-    addEventsDragAndDrop(item);
   });
 });
